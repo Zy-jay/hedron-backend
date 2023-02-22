02a66331f55bafac145ceda74e5100454ce8abf8
ianmarinovski <ianmarinovski@gmail.com>
2023-02-22 17:19:03 +0200

    add cors

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import cors from "cors"
 import express from "express"
 import mongoose from "mongoose"
 import { App } from "./src/App"
@@ -6,6 +7,7 @@ import router from "./src/types/router"
 // import router from "./src/types/router"
 
 const server = express()
+server.use(cors())
 server.use(express.json())
 server.use("/api", router)
 
