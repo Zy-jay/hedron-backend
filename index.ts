eb0e34413e96d72efe8670745e0210785a7942ba
ianmarinovski <ianmarinovski@gmail.com>
2023-02-24 22:04:30 +0200

    back

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
-import path from "path"
-import bodyParser from "body-parser"
 import cors from "cors"
 import express from "express"
 import mongoose from "mongoose"
-import serverless from "serverless-http"
 import { App } from "./src/App"
 import { DB_URL, PORT } from "./src/constants/network"
 import router from "./src/types/router"
@@ -12,25 +9,21 @@ import router from "./src/types/router"
 const server = express()
 server.use(cors())
 server.use(express.json())
-// server.get("/", (req, res) => {
-//   console.log(req.query)
-//   res.status(200).json("Good!")
-// })
-server.use(bodyParser.json())
-server.use("/.netlify/functions/server", router) // path must route to lambda
-server.use("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "./index.html")),
-)
+server.use("/api", router)
+server.get("/", (req, res) => {
+  console.log(req.query)
+  res.status(200).json("Good!")
+})
 async function startApp() {
   await mongoose.connect(DB_URL)
   mongoose.set("strictQuery", true)
   try {
-    server.listen(PORT, () => console.log("SERVER STARTED ON PORT " + PORT))
+    server.listen(undefined, () =>
+      console.log("SERVER STARTED ON PORT " + PORT),
+    )
     App()
   } catch (err) {
     console.log(err)
   }
 }
 startApp()
-
-module.exports.handler = serverless(server)
