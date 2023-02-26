f2d838aec7f1f125409d094b5fa3d0011f6cfdcb
ianmarinovski <ianmarinovski@gmail.com>
2023-02-26 18:14:52 +0200

    fix ts config

--- build/utils/compareString.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function compareStrings(stringA, stringB) {
-    return stringA.toLowerCase() === stringB.toLowerCase();
-}
-exports.default = compareStrings;
